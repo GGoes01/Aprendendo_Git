b1bff362a55833c1a221c58d4eaa2d26b30b668f
Gabriel <g.goes01@gmail.com>
2021-03-27 18:18:03 -0300

    Arquivo lalala

---
+++ lalala.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Coordenadas
+
+x = [0, 1, .5, 0]
+y = [0, 0, 1, 0]
+
+# Plot
+
+plt.figure(0, figsize=(8, 6))
+plt.plot(x, y, color='red')
+plt.title('Triângulo', fontsize='16')
+plt.show()
+
