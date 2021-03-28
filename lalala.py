ed808c678f1b96a4ce42cc8d59ca275123e02d33
user_b <g.goes01@gmail.com>
2021-03-28 00:55:48 -0300

    Ajuste na escala da imagem

--- lalala.py
+++ lalala.py
@@ -7,7 +7,7 @@ y = [0, 0, 1, 1, 0]
 
 # Plot
 
-plt.figure(0, figsize=(8, 6))
+plt.figure(0, figsize=(10, 7.5))
 plt.grid()
 plt.plot(x, y, color='red', linewidth=3)
 plt.title('Trapézio Vermelho', fontsize='16')
