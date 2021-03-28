9bc6596de9c43a330c5269ae0178a224e8fb8b70
user_b <g.goes01@gmail.com>
2021-03-28 00:43:37 -0300

    Mudando a cor da figura, o título, as coordenadas e adicionando grades.

--- lalala.py
+++ lalala.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 
 # Coordenadas
 
-x = [0, 1, .75, .25, 0]
+x = [0, 1, .8, .2, 0]
 y = [0, 0, 1, 1, 0]
 
 # Plot
 
 plt.figure(0, figsize=(8, 6))
-plt.plot(x, y, color='purple', linewidth=3)
-plt.title('Trapézio Roxo', fontsize='16')
+plt.grid()
+plt.plot(x, y, color='pink', linewidth=3)
+plt.title('Trapézio Rosa', fontsize='16')
 plt.show()
 
